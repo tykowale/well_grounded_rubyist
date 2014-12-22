a598401b322e70957baad40d6f8793ae4881aa80
Tyler Kowalewski <tykowale@gmail.com>
2014-12-22 17:56:21 -0600

    Basic person class

---
+++ missing_method.rb
@@ -0,0 +1,20 @@
+class Person
+  PEOPLE = []
+  attr_reader :name, :hobbies, :friends
+
+  def initialize(name)
+    @name = name
+    @hobbies = []
+    @friends = []
+    PEOPLE << self
+  end
+
+  def has_hobby(hobby)
+    @hobbies << hobby
+  end
+
+  def has_friend(friend)
+    @friends << friend
+  end
+
+end
