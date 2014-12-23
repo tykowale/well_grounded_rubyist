bf49a01230845a77678250dd984f96c039c317f4
Tyler Kowalewski <tykowale@gmail.com>
2014-12-22 18:04:36 -0600

    Method to find characteristics

--- missing_method.rb
+++ missing_method.rb
@@ -17,4 +17,20 @@ class Person
     @friends << friend
   end
 
+  def self.method_missing(m, *args)
+    method = m.to_s
+    if method.start_with?('all_with_')
+      attr = method[9..-1]
+      if self.public_method_defined?(attr)
+        PEOPLE.find_all do |person|
+          person.send(attr).include?(args[0])
+        end
+      else
+        raise ArguementError, "Can't find #{attr}"
+      end
+    else
+      super
+    end
+  end
+
 end
