6c52637fb893c7d48a810c3a8176cd2e513bc132
Tyler Kowalewski <tykowale@gmail.com>
2014-12-25 14:16:54 -0600

    Listing 5.4

--- learning_scope.rb
+++ learning_scope.rb
@@ -14,3 +14,27 @@ end
 
 c = C.new
 c.show_var
+
+class B
+  a = 5
+  module M
+    a = 4
+    module N
+      a = 3
+      class D
+        a = 2
+        def show_a
+          a = 1
+          puts a
+        end
+        puts a
+      end
+      puts a
+    end
+    puts a
+  end
+  puts a
+end
+
+d = B::M::N::D.new
+d.show_a
